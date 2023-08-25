72138b23b02749306c622e91cd80d31929eb3efd
nishi-design <68983522+nishi-design@users.noreply.github.com>
2023-08-25 21:35:08 +0530

    first change

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AiFillApple } from "react-icons/ai";
+
+ const Header=() =>{
+  return (
+    <div>
+        <div className="logo">
+        <AiFillApple />
+        </div>
+    </div>
+  )
+}
+export default Header;
+
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/public/index.html', './src/index.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
